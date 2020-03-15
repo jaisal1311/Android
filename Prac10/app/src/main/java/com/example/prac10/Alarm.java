bc54e2bc6bec143bea7d902e05b2a09779eb9023
jaisal1311 <jaisalashah123@gmail.com>
2020-03-15 10:37:34 +0530

    Working sql

---
+++ Prac10/app/src/main/java/com/example/prac10/Alarm.java
@@ -0,0 +1,14 @@
+package com.example.prac10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
---
+++ Prac10/app/src/main/java/com/example/prac10/MyBroad.java
@@ -0,0 +1,6 @@
+package com.example.prac10;
+
+import android.content.BroadcastReceiver;
+
+public class MyBroad extends BroadcastReceiver {
+}
